541238fb7438c4e7f738d577b42684f575f7276d
Diwaker Singh <diwaker.singh@inmobi.com>
2019-12-22 23:47:05 +0530

    initial ui commit

--- src/routes/gists/index.svelte
+++ src/routes/gists/index.svelte
@@ -18,24 +18,36 @@
     margin: 0 0 1em 0;
     line-height: 1.5;
   }
+  .gist-nav {
+    width: 20%;
+    overflow: hidden;
+    border-right: 1px solid salmon;
+  }
+  .gist-nav__item {
+    text-decoration: none;
+  }
+  a {
+    text-decoration: none;
+  }
 </style>
 
 <svelte:head>
   <title>Gists</title>
 </svelte:head>
+<div class="gist-nav">
+  <h1>Recent gists</h1>
 
-<h1>Recent gists</h1>
-
-<ul>
-  {#each gists as gist}
-    <!-- we're using the non-standard `rel=prefetch` attribute to
+  <ul>
+    {#each gists as gist}
+      <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-    <li>
-      <a rel="prefetch" href="gists/{gist.id}">
-        {gist.description || gist.html_url}
-      </a>
-    </li>
-  {/each}
-</ul>
+      <li class="gist-nav__item">
+        <a rel="prefetch" href="gists/{gist.id}">
+          {gist.description || gist.html_url}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</div>
